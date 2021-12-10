c361fad883d2c7659ea78926cef817a218a7ac83
Richard Chang <76931794+rddmonkey@users.noreply.github.com>
2021-12-09 22:40:54 -0800

    Update highestandlowest.py
    shortened the code using list comprehension

--- highestandlowest.py
+++ highestandlowest.py
@@ -1,23 +1,5 @@
 def high_and_low(numbers):
+    testlist = [int(x) for x in numbers.split()]
+    testlist.sort()
 
-    word = ""
-    low = float("inf")
-    high = float("-inf")
-
-    for x in numbers:
-        if x != " ":
-            word += x
-        if x == " ":
-            word = int(word)
-            if word > high:
-                high = word
-            if word < low:
-                low = word
-            word = ""
-    if word != "":
-        if int(word) > high:
-            high = word
-        if int(word) < low:
-            low = word
-
-    return f"{str(high)} {str(low)}"
+    return f"{testlist[-1]} {testlist[0]}"
